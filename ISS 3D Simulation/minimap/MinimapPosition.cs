a2706467f19975675ee0e5a0692466934a64c2c2
Lance <lance@lancesnider.com>
2017-09-08 14:23:02 -0700

    Add minimap

---
+++ ISS 3D Simulation/minimap/MinimapPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapPosition : MonoBehaviour {
+
+  public Transform earthParent;
+  public Transform earth;
+  private Transform miniEarthParent;
+  public Transform miniEarth;
+
+	// Use this for initialization
+	void Start () {
+    miniEarthParent = transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+    miniEarthParent.rotation = earthParent.rotation;
+    miniEarth.rotation = earth.rotation;
+	}
+}
